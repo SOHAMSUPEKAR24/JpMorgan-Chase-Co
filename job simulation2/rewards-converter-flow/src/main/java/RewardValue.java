985b66b47167b49c0915680b2bfcba8596d4d667
SOHAMSUPEKAR24 <sohamsupekar24@gmail.com>
2024-09-12 21:16:06 +0530

    task 1
    Completed task 1 of java job simulation lite

--- job simulation2/rewards-converter-flow/src/main/java/RewardValue.java
+++ job simulation2/rewards-converter-flow/src/main/java/RewardValue.java
@@ -1,2 +1,31 @@
 public class RewardValue {
+
+    // Constants
+    private static final double MILES_TO_CASH_RATE = 0.0035;
+
+    // Fields
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor for cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_RATE;
+    }
+
+    // Constructor for miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_RATE;
+    }
+
+    // Method to get cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
 }
